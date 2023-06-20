235ab1aeb313d7527d7b5f79de48ef87d466fa30
Ivan Martynov <ivanmartful@gmail.com>
2023-06-20 17:30:47 +0300

    quick hotfix

--- models/user.js
+++ models/user.js
@@ -4,24 +4,25 @@ const validator = require('validator');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: false,
     minlength: 2,
     maxlength: 30,
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
+    required: false,
     minlength: 2,
     maxlength: 30,
     default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: true,
+    required: false,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
-      validator: (url) => validator.isUrl(url),
+      // eslint-disable-next-line no-useless-escape
+      validator: (url) => url.match(/http(s)?:\/\/(ww.)?[a-z0-9\.\-]+\/[a-z0-9\.\-_~:\/?#\[\]@!$&'()*+,;=]+/gi) !== null,
       message: 'некорректная ссылка',
     },
   },
